599698d1cc3585b95e4fef119ec483ceb2523d5f
babysukuna <bb299436@gmail.com>
2022-05-05 23:18:48 +0800

    Updated code

--- main.b.c
+++ main.b.c
@@ -2,13 +2,13 @@
 int main(){
 	int age;
 	float income,tax;
-	printf("inptut the age:");
+	printf("nasaa oruulna uu:");
 	scanf("%d",&age);
-	printf("income if the age:");
+	printf("tsalin bas nas:");
 	scanf("%f",&income);
 	if(age > 18 && age < 55)
 		tax = income * 0.1;
 	else tax = 0;
-	printf("tax is %f",tax);	
+	printf("tatvar is %f",tax);	
 	return 0;
 }
